6907b4af3553449aca4d2b912f6c48c5ed4b6408
wmfeng <2734816259@qq.com>
2020-11-14 18:24:15 +0800

    8-1:什么是路由？如何使用路由："react-router-dom"

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Provider } from "react-redux";
+import { BrowserRouter, Route } from "react-router-dom";
 import Header from "./common/header/index";
 import store from "./store"
 
-function App() {
-  return (
-    <Provider store={store}>
-      <Header />
-    </Provider>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div>
+          <Header />
+          <BrowserRouter>
+            <div>
+              <Route path="/" exact render={() => <div>home</div>}></Route>
+              <Route path="/detail" exact render={() => <div>detail</div>}></Route>
+            </div>
+          </BrowserRouter>
+
+        </div>
+      </Provider>
+    );
+  }
+
 }
 
 export default App;
