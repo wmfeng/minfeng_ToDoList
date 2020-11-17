83d6ccfd53bb8bc950cb9a2f28929aec1a5bf6b1
wmfeng <2734816259@qq.com>
2020-11-17 21:08:54 +0800

    8-5:首页推荐部分代码编写

--- src/pages/home/store/reducer.js
+++ src/pages/home/store/reducer.js
@@ -5,12 +5,12 @@ const defaultState = fromJS({
         {
             id: 1,
             title: '社会热点',
-            imgUrl: 'https://upload-images.jianshu.io/upload_images/1900829-fcccf78fb618cc50?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240'
+            imgUrl: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1411604405,573366683&fm=26&gp=0.jpg'
         },
         {
             id: 2,
             title: '社会热点',
-            imgUrl: 'https://upload-images.jianshu.io/upload_images/1900829-fcccf78fb618cc50?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240'
+            imgUrl: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1411604405,573366683&fm=26&gp=0.jpg'
         }
     ],
     articleList:[
@@ -18,19 +18,29 @@ const defaultState = fromJS({
             id: 1,
             title: '11',
             desc: '2',
-            imgUrl: 'https://upload-images.jianshu.io/upload_images/1900829-fcccf78fb618cc50?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240'
+            imgUrl: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1411604405,573366683&fm=26&gp=0.jpg'
         },
         {
             id: 2,
             title: '11',
             desc: '2',
-            imgUrl: 'https://upload-images.jianshu.io/upload_images/1900829-fcccf78fb618cc50?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240'
+            imgUrl: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1449346651,454262593&fm=26&gp=0.jpg'
         },
         {
             id: 3,
             title: '11',
             desc: '2',
-            imgUrl: 'https://upload-images.jianshu.io/upload_images/1900829-fcccf78fb618cc50?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240'
+            imgUrl: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1449346651,454262593&fm=26&gp=0.jpg'
+        }
+    ],
+    recommendList: [
+        {
+            id: 1,
+            imgUrl: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3033438378,4156670815&fm=26&gp=0.jpg'
+        },
+        {
+            id: 2,
+            imgUrl: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3033438378,4156670815&fm=26&gp=0.jpg'
         }
     ]
 });
--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -79,3 +79,24 @@ export const ListInfo =	styled.div`
 		color: #999;
 	}
 `;
+
+export const RecommendWrapper = styled.div`
+	margin: 30px 0;
+	width: 280px;
+`;
+
+export const RecommendItem = styled.div`
+	width: 280px;
+	height: 50px;
+	background-image: url(${props=>props.imgUrl});
+	background-size: contain;
+`;
+
+export const WriterWrapper = styled.div`
+	width: 278px;
+	border: 1px solid #dcdcdc;
+	border-radius: 3px;
+	height: 300px;
+	line-height: 300px;
+	text-align: center;
+`;
