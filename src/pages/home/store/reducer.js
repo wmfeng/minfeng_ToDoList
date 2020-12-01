8fdbf2c2bc962cf267f92121fcd624f32c3ca1ad
wmfeng <2734816259@qq.com>
2020-12-01 18:55:10 +0800

    8-8:加载实现更多功能

--- src/pages/home/store/reducer.js
+++ src/pages/home/store/reducer.js
@@ -47,20 +47,28 @@ const defaultState = fromJS({
     articlePage: 1
 });
 
+const changeHomeData = (state,action)=>{
+    return state.merge({
+        topicList: fromJS(action.topicList),
+        articleList: fromJS(action.articleList),
+        recommendList: fromJS(action.recommendList),
+    })
+};
+
+const addArticleList = (state,action)=>{
+    return state.merge({
+        articleList: state.get('articleList').concat(action.list),
+        articlePage: action.nextPage
+    })
+};
+
 export default (state = defaultState, action) => {
     switch (action.type) {
         case constants.CHANGE_HOME_DATA:
-            return state.merge({
-                topicList: fromJS(action.topicList),
-                articleList: fromJS(action.articleList),
-                recommendList: fromJS(action.recommendList),
-            })
+            return changeHomeData(state,action)
             case constants.ADD_ARTICLE_LIST:
                 // return state.set('articleList',state.get('articleList').concat(action.list))
-                return state.merge({
-                    articleList: state.get('articleList').concat(action.list),
-                    articlePage: action.nextPage
-                })
+                return addArticleList(state,action)
         default:
             return state;
     }
