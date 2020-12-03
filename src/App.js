f5317f1d2ad65836ddffb0bfd1f56e09d3b3c5d0
wmfeng <2734816259@qq.com>
2020-12-03 20:15:55 +0800

    9-5登陆页面布局

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Provider } from "react-redux";
 import { BrowserRouter, Route } from "react-router-dom";
 import Header from "./common/header/index";
 import Home from "./pages/home";
+import Login from "./pages/login";
 import Detail from "./pages/detail";
 import store from "./store"
 
@@ -15,6 +16,7 @@ class App extends Component {
           <div>
             <Header />
             <Route path="/" exact component={Home}></Route>
+            <Route path="/login" exact component={Login}></Route>
             <Route path="/detail/:id" exact component={Detail}></Route>
             {/* <Route path="/" exact render={() => <div>home</div>}></Route>
               <Route path="/detail" exact render={() => <div>detail</div>}></Route> */}
