8a2c85ab0ca317ae043b48d7b7aeffa761e83024
wmfeng <2734816259@qq.com>
2020-12-05 22:18:02 +0800

    9-6:登陆功能实现

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { Link } from 'react-router-dom';
 import { CSSTransition } from "react-transition-group";
 import { actionCreators } from "./store";
+import { actionCreators as loginActionCreators } from '../../pages/login/store';
 import {
     HeaderWrapper,
     Logo,
@@ -63,7 +64,7 @@ class Header extends Component {
         }
     }
     render() {
-        const { focused, handleInputFocus, handleInputBlur, list } = this.props;
+        const { focused, handleInputFocus, handleInputBlur, list, login, logout } = this.props;
         return (
             <HeaderWrapper>
                 <Link to="/">
@@ -72,7 +73,12 @@ class Header extends Component {
                 <Nav>
                     <NavItem className="left active">首页</NavItem>
                     <NavItem className="left">下载App</NavItem>
-                    <NavItem className="right">登陆</NavItem>
+                    {
+                        login ? 
+                        <NavItem onClick={logout} className="right">退出</NavItem> : 
+                        <Link to="/login"><NavItem className="right">登陆</NavItem></Link>
+                    }
+
                     <NavItem className="right">
                         <i className="iconfont">&#xe636;</i>
                     </NavItem>
@@ -114,7 +120,8 @@ const mapStateToProps = (state) => {
         list: state.getIn(['header', 'list']),
         page: state.getIn(["header", "page"]),
         mouseIn: state.getIn(["header", "mouseIn"]),
-        totalPage: state.getIn(['header', 'totalPage'])
+        totalPage: state.getIn(['header', 'totalPage']),
+        login: state.getIn(['login', 'login'])
     }
 };
 
@@ -146,7 +153,9 @@ const mapDispatchToProps = (dispatch) => {
             } else {
                 dispatch(actionCreators.changePage(1))
             }
-
+        },
+        logout(){
+            dispatch(loginActionCreators.logout())
         }
     }
 }
